7dbd25b5c05bb2c9a2e09824c468330aad8b7886
Charlotte Willens <charwillens@gmail.com>
2016-11-09 16:36:31 -0800

    Create controller programs for database collections i Please enter the commit message for your changes. Lines starting

---
+++ server/db/controllers/transactionController.js
@@ -0,0 +1,1 @@
+var Transaction = require('../models/transactionModel');
--- server/db/models/charityModel.js
+++ server/db/models/charityModel.js
@@ -2,8 +2,15 @@ var db = require('../config/db');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+//we can add any other relevant categories based on orghunter api output
 var charitySchema = new Schema({
   name: String,
+  category: String, 
+  ein: String,
+  donationUrl: String,
+  city: String,
+  state: String,
+  zipCode: String,
   balanceOwed: Number
 });
 

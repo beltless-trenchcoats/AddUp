0b421c00a3cebc38c468016aea605c5254ce6557
Charlotte Willens <charwillens@gmail.com>
2016-11-11 12:33:46 -0800

    Create user and login user functions interacting with database

--- server/db/controllers/users.js
+++ server/db/controllers/users.js
@@ -1,48 +1,69 @@
 var db = require('../config/db');
+var bcrypt = require('bcrypt');
 
-var username='helga';
-var password='test';
+exports.createUser = function(username, password, callback) {
+  db.query({
+    text: 'SELECT username, password FROM users \
+      WHERE username = \'' + username + '\';'
+  }, 
+  function(err, rows) {
+    if (err) {
+      console.log(err);
+    } else if (rows.rowCount > 0) {
+      console.log('user already exists', rows.rows);
+      callback('user already exists');
+    } else {
+      console.log('found nothing');
+      bcrypt.hash(password, 10, function(err, hash) {
+        // Store hash in your password DB. 
+        console.log('hash', hash);
+        db.query({
+          text: 'INSERT INTO users(username, password) \
+            VALUES($1, $2)',
+          values: [username, hash]
+        },
 
-// pool.connect(function(err, client, done) {
-//   if(err) {
-//     return console.error('error fetching client from pool', err);
-//   }
-// pool.query({
-//   text: 'INSERT INTO users(username, password) \
-//     VALUES($1, $2)',
-//   values: [username, password]
-// },
+        function(err, result) {
+          if (err) {
+            console.log('ERROR IN THE INSERT', err);
+          } else {
+            console.log('SUCCESS');
+            callback('success');
+          }
+        });
+      });
+    }
+  });
+};
 
-// function(err, result) {
-//   if (err) {
-//     console.log('ERROR IN THE INSERT', err);
-//   } else {
-//     console.log('SUCCESS');
-//   }
+exports.loginUser = function(username, password, callback) {
+  db.query({
+    text: 'SELECT username, password FROM users \
+      WHERE username = \'' + username + '\';'
+  }, 
+  function(err, rows) {
+    if (err) {
+      console.log(err);
+    } else if (rows.rowCount > 0) {
+      if (username === rows.rows[0].username) {
+        bcrypt.compare(password, rows.rows[0].password, function(err, res) {
+            // res == true 
+            if (err) {console.log(err);}
+            if (res) {
+              callback(true);
+            }
+        }); 
+      }
+    } else {
+      console.log('found nothing');
+      callback(false);
+    }
+  });
+};
+
+// exports.createUser('herbert', 'test', function(response) {
+//   console.log(response);
 // });
-
-db.query({
-  text: 'SELECT username, password, plaid_access_token FROM users \
-    WHERE username = \'' + username + '\';'
-}, 
-
-function(err, rows) {
-  if (err) {
-    console.log(err);
-  }
-  else if (rows.rowCount > 0) {
-    console.log(rows.rows);
-  } else {
-    console.log('found nothing');
-//   // client.query('SELECT $1::int AS number', ['1'], function(err, result) {
-//   //   //call `done()` to release the client back to the pool 
-//   //   done();
- 
-//   //   if(err) {
-//   //     return console.error('error running query', err);
-//   //   }
-//   //   console.log(result.rows[0].number);
-//   //   //output: 1 
-//   // });
-  }
-});
+// exports.checkUser('herbert', 'test', function(response) {
+//   console.log(response);
+// });

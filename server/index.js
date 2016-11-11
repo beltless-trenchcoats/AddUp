90d8c88d74f988b59f524f3d399fe911b7b92e88
MythlinMasterdo <miles71397@gmail.com>
2016-11-11 09:16:34 -0800

    updates to index.js

--- server/index.js
+++ server/index.js
@@ -3,18 +3,20 @@ var parser = require('body-parser');
 var plaid = require('plaid');
 
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 
-app.use(bodyParser.json());
+app.use(parser.json());
 
 app.get('/accounts', function(req, res, next) {
   var public_token = req.query.public_token;
+  console.log('public token', public_token);
 
   plaidClient.exchangeToken(public_token, function(err, tokenResponse) {
     if (err != null) {
       res.json({error: 'Unable to exchange public_token'});
     } else {
       var access_token = tokenResponse.access_token;
+      console.log('access_token', access_token);
       //save access_token with user account in the db
     }
   });

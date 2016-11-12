eccd3612f0ef5eff9505a42a00a4c9da6a943ffb
Karina Kinaman <kkinaman419@gmail.com>
2016-11-12 14:37:59 -0800

    (refactor) Rename helpers.js to worker.js and finalize testing for worker functions

--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -1,6 +1,6 @@
 var expect = require('chai').expect;
 
-var helpers = require('../server/helpers');
+var worker = require('../server/worker');
 
 describe('Helper functions', function() {
 
@@ -17,8 +17,8 @@ describe('Helper functions', function() {
     },
     {
       email: 'miles71397@gmail.com',
-      plaid_access_token: 'test_wells',
-      stripe_bank_account_token: 'btok_9YDoZt3NHiIjun',
+      plaid_access_token: 'test_bofa',
+      stripe_bank_account_token: 'btok_9YEdF4atq1rpif',
       pending_balance: 0,
       monthly_total: 25,
       monthly_limit: 25,
@@ -50,30 +50,30 @@ describe('Helper functions', function() {
 
   describe('finding recent transactions', function() {
     it('should only return transactions since the last transaction id', function(done) {
-      expect(helpers.findRecentTransactions(users[0], transactions).length).to.equal(2);
-      expect(helpers.findRecentTransactions(users[1], transactions).length).to.equal(1);
+      expect(worker.findRecentTransactions(users[0], transactions).length).to.equal(2);
+      expect(worker.findRecentTransactions(users[1], transactions).length).to.equal(1);
       done();
     });
   });
 
   describe('rounding up transactions', function() {
     it('should return rounded-up amount for an amount > 0.50', function(done) {
-      expect(helpers.roundUpTransaction(users[0], transactions[0])).to.equal(0.51);
+      expect(worker.roundUpTransaction(users[0], transactions[0])).to.equal(0.51);
       done();
     });
 
     it('should only return an amount up to their monthly limit', function(done) {
-      expect(helpers.roundUpTransaction(users[0], transactions[1])).to.equal(users[0].monthly_limit - users[0].monthly_total);
+      expect(worker.roundUpTransaction(users[0], transactions[1])).to.equal(users[0].monthly_limit - users[0].monthly_total);
       done();
     });
 
     it('should return 0 if they are already over their monthly limit', function(done) {
-      expect(helpers.roundUpTransaction(users[1], transactions[0])).to.equal(0);
+      expect(worker.roundUpTransaction(users[1], transactions[0])).to.equal(0);
       done();
     });
 
     it('should update a users pending balance if their aggregate balance is still < 0.50', function(done) {
-      expect(helpers.roundUpTransaction(users[0], transactions[2])).to.equal(0);
+      expect(worker.roundUpTransaction(users[0], transactions[2])).to.equal(0);
       //giving time for the inner database call to go through 
       setTimeout(() => done(), 1000);
       // done();
@@ -83,7 +83,7 @@ describe('Helper functions', function() {
   describe('charging with stripe', function() {
     it('should return success when charging a user a given amount', function(done) {
       var testCharge = function() {
-        helpers.charge(users[0], 0.51);
+        worker.charge(users[0], 0.51, () => console.log());
       }
       expect(testCharge).to.not.throw(Error);
       setTimeout(() => done(), 1000);

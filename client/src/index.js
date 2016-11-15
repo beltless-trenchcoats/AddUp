825cef9ace5fc1c0d25aed8c7b77a6eb4ee1c011
Tim Garibaldi <tgaribal@gmail.com>
2016-11-14 16:18:45 -0800

    Setup link in search page to link to charity profile page

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { Router, Route, browserHistory } from 'react-router';
 import App from './App';
 import UserProfile from './userProfile';
 import SearchPage from './SearchPage';
+import CharityProfilePage from "./CharityProfilePage";
 import './index.css';
 
 ReactDOM.render (
@@ -12,6 +13,7 @@ ReactDOM.render (
 		<Route path="/" component={App}/>
 	    <Route path="/user" component={UserProfile} />
 	   	<Route path="/search" component={SearchPage} />
+	   	<Route path="/charity/:id" component={CharityProfilePage} />
 	</Router>,
   document.getElementById('root')
 );

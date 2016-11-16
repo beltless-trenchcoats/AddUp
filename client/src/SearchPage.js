7df80a22a8d28a26ab6b0278be155df52f8b97a3
Miles Smith <milessmith@Miless-MacBook-Pro.local>
2016-11-15 19:09:56 -0800

    dynamiclly load search results based on page selection

--- client/src/SearchPage.js
+++ client/src/SearchPage.js
@@ -70,46 +70,61 @@ class SearchPage extends Component {
     })
   }
 
+  //this function is called by ReactPaginate component
   pageSelect = (data) => {
+    //previous start is what rows to request from the api
     var previousStart = this.state.start;
+    //activePage is the current selected page
     var activePage = data.selected;
+    //lastActivePage is the last page the user selected
     var lastActivePage = this.state.lastPage;
-    console.log('previousStart !!!!!!!!!!!!!!!!!!!!!', previousStart);
-    console.log('lastpage ', lastActivePage);
-    console.log('activePage', activePage);
+    //on first select if statement will be called, it moves activePage up but leaves
+    //lastPage at 1 still
     if(activePage >= lastActivePage && this.state.firstPageChange === false) {
-      console.log('first if');
+      //sets activePage state up 1 and firstPageChange state to false
       this.setState({activePage: activePage += 1, start: previousStart += 20, firstPageChange: true},
         function() {
+          //gets results from API
           this.getResults.call(this);
+          //maps over results and re renders them
           this.state.searchResults.map((charity, i) =>
           <CharitySearchResult key={i} info={charity} />)
         })
-      } else if(activePage > lastActivePage) {
-        console.log('2nd if');
-          this.setState({activePage: activePage+= 1, start: previousStart += 20, lastPage: lastActivePage += 1},
-            function() {
-              this.getResults.call(this);
-              this.state.searchResults.map((charity, i) =>
-              <CharitySearchResult key={i} info={charity} />)
-            });
-      } else if(activePage === lastActivePage) {
-        console.log('2nd if');
-        this.setState({activePage: activePage -= 1, start: previousStart -= 20},
+    //gets called when the user increments up in numbers like 1 -> 2
+    } else if(activePage > lastActivePage) {
+      //pageDifference is if the user skips from 1 -> 9 so we can Calculate where to
+      //start the API call
+      var pageDifference = activePage - lastActivePage;
+      //resultDifference uses pageDifference and multiplies it by 20
+      var resultDifference = pageDifference * 20;
+        this.setState({activePage: activePage += pageDifference, start: previousStart += resultDifference,
+          lastPage: lastActivePage += pageDifference},
           function() {
             this.getResults.call(this);
             this.state.searchResults.map((charity, i) =>
             <CharitySearchResult key={i} info={charity} />)
           });
-      } else {
-    console.log('last if');
-    this.setState({activePage: activePage -= 1, start: previousStart -= 20, lastPage: lastActivePage -= 1},
+    //this route is for when users go down 1 by 1, eventually lastActivePage and activePage will be =
+    } else if(activePage === lastActivePage) {
+      this.setState({activePage: activePage -= 1, start: previousStart -= 20},
+        function() {
+          this.getResults.call(this);
+          this.state.searchResults.map((charity, i) =>
+          <CharitySearchResult key={i} info={charity} />)
+        });
+    //else if the user is making steps down 5 -> 4, etc...
+    } else {
+      //same concept as stepping up but in reverse order
+      var pageDifference = lastActivePage - activePage;
+      var resultDifference = pageDifference * 20;
+      this.setState({activePage: activePage -= pageDifference, start: previousStart -= resultDifference,
+         lastPage: lastActivePage -= pageDifference},
       function() {
         this.getResults.call(this);
         this.state.searchResults.map((charity, i) =>
         <CharitySearchResult key={i} info={charity} />)
-    });
-  }
+      });
+    }
 }
 
   render() {

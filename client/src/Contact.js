943c3f0247826739b493ae05773779a9a8eeabfe
Miles Smith <milessmith@Miless-MacBook-Pro.local>
2016-11-18 17:11:36 -0800

    create about and contact us buttons and link them to new component

---
+++ client/src/Contact.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Header from './Header';
+
+
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <p>hello world</p>
+      </div>
+    )
+  }
+}
+
+export default Contact;

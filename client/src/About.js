303ea3ddb733d89dfc40731d634514de582776ec
Miles Smith <milessmith@Miless-MacBook-Pro.local>
2016-11-18 19:03:51 -0800

    add paragraph to about

--- client/src/About.js
+++ client/src/About.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { Button, Jumbotron, Col, Panel } from 'react-bootstrap';
 import Header from './Header';
-
+import './App.css';
 
 class About extends Component {
   constructor(props) {
@@ -14,7 +15,21 @@ class About extends Component {
     return (
       <Header>
         <div>
-          <p>hello world</p>
+          <div className="aboutUsSection">
+
+            <Col xs={6} md={4} className="infoColumn">
+              <Panel header="About Us!" bsStyle="primary">
+                <p>AddUp++ aggregates charitable donations through automatic
+                    micropayments by rounding up each credit or debit transaction
+                    to the next dollar and sending the change to a charity of your
+                    choice. Discover and select from any IRS-recognized charitable
+                    organization or user-generated crowd-funding initiative. Alleviate
+                    any financial concerns by setting monthly donation limits, and track
+                    your donation history with our mobile app.
+                </p>
+              </Panel>
+            </Col>
+          </div>
         </div>
       </Header>
     )

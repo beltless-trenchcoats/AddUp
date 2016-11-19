2ef4643a10a1d69be0f7eb5c5bdb01427d71a222
Miles Smith <milessmith@Miless-MacBook-Pro.local>
2016-11-18 19:32:54 -0800

    create custom favicon

--- client/src/Contact.js
+++ client/src/Contact.js
@@ -5,6 +5,7 @@ class Contact extends Component {
   render () {
     return (
       <Header>
+<<<<<<< HEAD
         <div className="contactUsSection">
           <div className='contact'>
             <p>Email: support@AddUp.com</p>

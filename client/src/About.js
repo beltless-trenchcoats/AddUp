e8c91499bc1253c2f6b4af796fdbfc49366c1716
MythlinMasterdo <miles71397@gmail.com>
2016-11-22 12:03:44 -0800

    adjust about us panel size

--- client/src/About.js
+++ client/src/About.js
@@ -19,7 +19,7 @@ class About extends Component {
         <div>
           <div className="aboutUsSection">
 
-            <Col xs={6} md={12} className="infoColumn">
+            <Col xs={6} md={8} className="infoColumn">
               <Panel header="About Us!" bsStyle="primary">
                 <p>AddUp++ aggregates charitable donations through automatic
                     micropayments by rounding up each credit or debit transaction

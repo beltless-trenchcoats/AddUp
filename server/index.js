23b7f9991a6ffda752afa401b2f7e9bf3c7457a4
Charlotte Willens <charwillens@gmail.com>
2016-11-21 18:54:54 -0800

    Start setting up stripe for users with custom causes. Waiting to hear back from stripe before proceeding.

--- server/index.js
+++ server/index.js
@@ -16,6 +16,9 @@ var charitiesDB = require('./db/controllers/charities');
 var helperFunctions = require('./helpers');
 var aws = require('aws-sdk');
 var S3_BUCKET = process.env.S3_BUCKET || 'addupp-profile-photos';
+// var stripe = require("stripe")(
+//   "sk_test_eKJNtjs3Il6V1QZvyKs1dS6y"
+// );
 
 var app = express();
 var port = process.env.PORT || 8080;
@@ -140,6 +143,50 @@ app.post('/api/plaid/transactions', function(req, res) {
     res.send(resp.data)
   })
     .catch(err => console.log('error pinging plaid', err));
+});
+
+//
+app.post("/oauth/callback", function(req, res) {
+
+  var code = req.body.code;
+
+  // Make /oauth/token endpoint POST request
+  request.post({
+    url: 'https://connect.stripe.com/oauth/token',
+    form: {
+      grant_type: "authorization_code",
+      client_id: 'ca_9bfGnqU5JGVlcbNEJfmcmCCDAewlhQP7',
+      code: code,
+      client_secret: 'sk_test_eKJNtjs3Il6V1QZvyKs1dS6y'
+    }
+  }, function(err, r, body) {
+    if (err) {
+      console.log(err);
+    }
+    console.log('STRIPE BODY', body);
+    var accessToken = JSON.parse(body).access_token;
+    var stripeUserID = JSON.parse(body).stripe_user_id;
+
+    // Do something with your accessToken
+    stripe.charges.create({
+      amount: .01,
+      currency: "usd",
+      source: accessToken, // obtained with Stripe.js
+      description: "TEST CHARGE"
+    }, {
+      stripe_account: stripeUserID
+    }, function(err, charge) {
+      // asynchronously called
+      if (err) {
+        console.log(err);
+      }
+      console.log('IT CHARGED', charge);
+    });
+
+    // For demo"s sake, output in response:
+    res.send({ "Your Token": accessToken });
+
+  });
 });
 
 //signup new users to our local db

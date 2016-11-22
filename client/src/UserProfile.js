3aadf7232cbd86befaf220e587b134254b9fede9
MythlinMasterdo <miles71397@gmail.com>
2016-11-21 19:39:29 -0800

    remove test data

--- client/src/UserProfile.js
+++ client/src/UserProfile.js
@@ -15,9 +15,6 @@ import ChangeEmailModal from './ChangeEmailModal';
 import ChangePasswordModal from './ChangePasswordModal';
 
 let transactionChartData = [];
-
-const data = [{name: 'Group A', value: 400}, {name: 'Group B', value: 300},
-                  {name: 'Group C', value: 300}, {name: 'Group D', value: 200}];
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
 const RADIAN = Math.PI / 180;

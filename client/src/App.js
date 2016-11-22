dc922d66da7c984e4080ff648918e2daa8b2e77e
Charlotte Willens <charwillens@gmail.com>
2016-11-22 10:45:08 -0800

    Modify custom cause search so it is not case sensitive, start chart on home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import { Button, Jumbotron, Col, Panel } from 'react-bootstrap';
+import { Row, Button, Jumbotron, Col, Panel } from 'react-bootstrap';
 import axios from 'axios';
+import { AreaChart, Area, XAxis, YAxis, LineChart, Line, Legend, Bar, CartesianGrid, Tooltip, BarChart} from 'recharts';
 
 import './App.css';
 
@@ -11,7 +12,8 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      userSession: {}
+      userSession: {},
+      data: []
     }
   }
 
@@ -26,7 +28,38 @@ class App extends Component {
     .catch((err) => {
       console.log(err);
     });
+
+    axios.get('http://localhost:8080/api/transactions/all')
+    .then((res) => {
+      // var data = res.data.map(function(elt) {
+      //   return {name: elt.name, total_donated: elt.total_donated, category: elt.category};
+      // });
+      // this.setState({data: data});
+      console.log('all', res.data);
+      var transactions = res.data;
+      var daysData = [];
+      transactions.forEach(function(elt) {
+        var day = new Date(elt.date_time);
+        var dayString = day.getMonth() + 1 +'/' + day.getDate() + '/' + day.getFullYear();
+        console.log(dayString, elt.date_time);
+        daysData[dayString] = daysData[dayString] + elt.amount || elt.amount;
+      });
+      var data = [];
+      for (var key in daysData) {
+        data.push({name: key, amount: daysData[key]});
+      }
+      console.log('daysData', data);
+      this.setState({data: data});
+      // this.setState({
+      //   userSession: res.data
+      // })
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   }
+
+  
 
   render() {
     return (
@@ -61,6 +94,15 @@ class App extends Component {
               </Panel>
             </Col>
 
+
+            <Row>
+              <LineChart width={600} height={400} data={this.state.data}>
+                <XAxis dataKey="name"/>
+                <YAxis/>
+                <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
+                <Line type="monotone" dataKey="amount" stroke="#8884d8" />
+              </LineChart>
+            </Row>
             <div className="footer">
               
             </div>

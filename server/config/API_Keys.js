8fc0946d5eb1b6b857d54b3c796d7efd37468cb9
Charlotte Willens <charwillens@gmail.com>
2016-11-29 14:32:00 -0800

    Made active page update automatically in search pagination

---
+++ server/config/API_Keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  orgHunter: '5473ac682c52543bcf470af956205eec',
+  gmaps: 'AIzaSyCHvNdiM0yneOhenX-iTqBdSN2Nxx1FL5g',
+  AWSAccessKeyId: 'AKIAIHX3XNJGLAWZ7J3Q',
+  AWSSecretKey:'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',
+  payPalClientID: 'AbKfqUOHESc5PY4gDXg48J1SWpFklwCmBT1iqR8XCIic7ERMSvTltHSl-vecHNjuJqDwSSAALKieI3UL',
+  payPalSecret: 'EPov8thnjbKLZz2lyGTFo63gAS6TQ0OzbVJcs0eJDu75vKDctiI29F9NipaEgTO8NMYnLRZzwOfdPmf3'
+};

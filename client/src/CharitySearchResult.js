d4b468c86982611a27303a574b86b08311562746
MythlinMasterdo <miles71397@gmail.com>
2016-11-28 16:53:29 -0800

    maybe fix merge issues

--- client/src/CharitySearchResult.js
+++ client/src/CharitySearchResult.js
@@ -15,13 +15,8 @@ class CharitySearchResult extends Component {
     return (
      <div>
      {/* {console.log("charity", this.props.info)} */}
-<<<<<<< HEAD
-      <Panel className="charityCard" header={<a href={"/" + this.state.type + '/' + this.state.charityId}>{this.props.info.charityName}</a>} bsStyle="info">
-        <p className="category">{this.props.info.category}</p>
-=======
       <Panel className="charityCard" header={<a href={"/" + this.state.type + '/' + this.props.info.ein}>{this.props.info.charityName}</a>} bsStyle="info">
         <p className="category">{this.props.info.category + ' type:' + this.props.info.type}</p>
->>>>>>> pagination clicks work
         <p className="missionStatement"><span className="missionStatementTitle">Mission Statement: </span>{this.props.info.missionStatement}</p>
         <p className="location">{this.props.info.city}, {this.props.info.state}</p>
         <p className="website">{this.props.info.website}</p>

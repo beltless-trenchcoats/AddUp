d08504786cf569d53da721e797187d8be7783ad1
Tim Garibaldi <tgaribal@gmail.com>
2016-11-29 17:16:50 -0800

    Removed API keys as they are now stored in heroku environment

--- server/config/API_Keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	orgHunter: '5473ac682c52543bcf470af956205eec',
-	gmaps: 'AIzaSyCHvNdiM0yneOhenX-iTqBdSN2Nxx1FL5g',
-	AWSAccessKeyId: 'AKIAJCSZ7TMDVT2C7GJA',
-	AWSSecretKey: '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'
-};

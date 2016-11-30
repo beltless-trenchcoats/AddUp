33d4b09370241a5d6f81b315012b52eeb2e9b4f2
MythlinMasterdo <miles71397@gmail.com>
2016-11-30 11:01:38 -0800

    create protected routes

--- client/src/index.js
+++ client/src/index.js
@@ -10,18 +10,36 @@ import CharityProfilePage from "./CharityProfilePage";
 import About from "./About";
 import Contact from "./Contact";
 import CustomCauseProfilePage from "./CustomCauseProfile";
+import helpers from '../helpers';
+
+function loggedIn() {
+	var cookies = helpers.parseCookie(document.cookie);
+	if(cookies.email !== undefined) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function isAuth (nextState, replace) {
+	if(!loggedIn()) {
+		replace({
+			pathname: '/'
+		})
+	}
+}
 
 
 ReactDOM.render (
 	<Router history={browserHistory}>
 		<Route path="/" component={App}/>
-	    <Route path="/user" component={UserProfile} />
+	    <Route path="/user" component={UserProfile} onEnter={isAuth} />
 	    <Route path="/profile/:id" component={PublicProfile} />
 	   	<Route path="/search" component={SearchPage} />
 			<Route path="/about" component={About} />
 			<Route path="/contact" component={Contact} />
 	   	<Route path="/:type/:id" component={CharityProfilePage} />
-      <Route path="/myCause/edit/:id" component={CustomCauseProfilePage} />
+      <Route path="/myCause/edit/:id" component={CustomCauseProfilePage} onEnter={isAuth} />
 	</Router>,
   document.getElementById('root')
 );

e0d9777630b5561788a07152d45877277a709ef6
Karina Kinaman <kkinaman419@gmail.com>
2016-11-30 20:15:42 -0800

    (styling) Begin styling charity profile page

--- client/src/App.js
+++ client/src/App.js
@@ -111,19 +111,19 @@ class App extends Component {
 
               <Col xs={6} md={4} className="impactColumn">
                 <Panel header="Every Month Your Change Can" bsStyle="primary" className="impactPanel">
-                  <p>Protect 12 people from Malaria for three to four years, by giving to the <span className="charityName">Against Malaria Foundation</span>.</p>
+                  <p>Protect 12 people from Malaria for three to four years, by giving to the <span className="emphasis">Against Malaria Foundation</span>.</p>
                 </Panel>
               </Col>
 
               <Col xs={6} md={4} className="impactColumn">
                 <Panel header="Every 6 Months Your Change Can" bsStyle="primary" className="impactPanel">
-                  <p>Provide safe water to 142 community members for one year with <span className="charityName">Evidence Action</span>.</p>
+                  <p>Provide safe water to 142 community members for one year with <span className="emphasis">Evidence Action</span>.</p>
                 </Panel>
               </Col>
 
               <Col xs={6} md={4} className="impactColumn">
                 <Panel header="Every Year Your Change Can" bsStyle="primary" className="impactPanel">
-                  <p>Provide school meal programs to 5 children for one year with <span className="charityName">Oxfam</span>.</p>
+                  <p>Provide school meal programs to 5 children for one year with <span className="emphasis">Oxfam</span>.</p>
                 </Panel>
               </Col>
 
